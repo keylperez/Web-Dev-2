438d328b9ee307f7c15ec2807836ea423d9dd8c4
keylperez <perez.williamcaleb@gmail.com>
2021-09-05 00:04:34 +0800

    bug fixes and improvement

--- ArrayActivity/script.js
+++ ArrayActivity/script.js
@@ -1,3 +1,5 @@
+// 'use strict';
+
 //creates arrays
 let names = [];
 let tempNames = [];
@@ -12,7 +14,7 @@ function addUsername(){
         window.alert("Field has no value");
         return;
     }
-    console.log("username: "+username);
+    console.log("username: "+ username);
     userInput.value = "";//resets textbox value to 0
     names.push(username);//appends username to array
     tempNames.push(username);
@@ -22,8 +24,8 @@ function addUsername(){
 function checkUsername() {
     if(tempNames.length === 0) return;//if tempNames is empty, exit function
     tempNames.forEach(el => {//loops through names array
-        if(el.length > el) invalid.push(el);//adds invalid username to invalid array
-        if(el.length <= el) valid.push(el);//adds valid username to valid array
+            if(el.length > 10) invalid.push(el);//adds invalid username to invalid array
+            if(el.length <= 10) valid.push(el);//adds valid username to valid array
         
     });
     
@@ -33,11 +35,12 @@ function checkUsername() {
     // }
 
     displayTable();//display table after segregation
-    //resets array to avoid duplicates
+    //resets array to avoid duplicates\
+    // console.log("checkUsername function arrays: " + tempNames + valid + invalid);
     invalid = [];
     valid = [];
     tempNames = [];
-    console.log("checkUsername function arrays: " + tempNames + valid + invalid);
+    // console.log("checkUsername function arrays: " + tempNames + valid + invalid);
     
 
 };
@@ -45,19 +48,13 @@ function checkUsername() {
 function displayTable(){
     const table1 = document.querySelector("#table1");//gets table 1 element
     const table2 = document.querySelector("#table2");//gets table 2 element
-
+    console.log(valid.length);
     valid.forEach(el => {//loops through valid array
+        console.log('success');
         table1.innerHTML += `<h3>${el}</h3>`;//adds username in cell of table
     });
     invalid.forEach(el => {//loops through valid array
+        console.log('success');
         table2.innerHTML += `<h3>${el}</h3>`;//adds username in cell of table
-    });
-    
-    
-    // for(let i = 0; i < valid.length; i++){
-    //     table1.innerHTML += `<tr><tc><h3>${valid[i]}</h3></tc></tr>`;//adds username in cell of table
-    // }
-    // for(let i = 0; i < invalid.length; i++){
-    //     table2.innerHTML += `<tr><tc><h3>${invalid[i]}</h3></tc></tr>`;//adds username in cell of table
-    // }
+    }); 
 };
